d74f44237fe916976de95e61463aa54dd65d458b
nazmulh6 <nazmulusama890@gmail.com>
2026-03-13 13:16:51 +0600

    make it responsive

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # Question & Answer
 
-## What is JSX, and why is it used?
+### What is JSX, and why is it used?
 JSX means "JavaScript XML" is a syntax extension for JavaScript that allows developers to write HTML-code directly within their JavaScript code.
-### Uses:
+#### Uses:
 for safety, readability, maintainability and clean code.
 
-## What is the difference between State and Props?
+### What is the difference between State and Props?
 
-### State:
+#### State:
 - Data is managed within the component itself.
 - State is mutable and can be changed within the component using an updater function (e.g., setState or useState).
-### Props:
+#### Props:
 - Data is passed from a parent component to a child component.
 - Props are immutable and read-only; they cannot be modified by the receiving component.
 
-## What is the useState hook, and how does it work?
+### What is the useState hook, and how does it work?
 
-### useState hook: 
+#### useState hook: 
 
 The useState hook is a built-in React function that allows functional components to manage and track state.
 
-### how it works:
+#### how it works:
 
 If we call useState at the top level of your function component, passing the initialState as an argument.
 
@@ -28,12 +28,12 @@ It returns an array with two elements: the current state value and a function (o
 
 When the state updater function is called with a new value, React re-renders the component with the new state, preserving the state value across renders.
 
-## How can you share state between components in React?
+### How can you share state between components in React?
 
 We can share state between components by passing as props in the component tag name then destructuring from the component function.
 
 
-## How is event handling done in React?
+### How is event handling done in React?
 
 Event handling in React is similar to HTML but with some key differences. 
 
--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -3,43 +3,54 @@ import dateImg from '../../assets/Vector-date-box.jpg'
 import { toast } from 'react-toastify';
 
 
-const Ticket = ({ ticket ,selectedTicket, setSelectedTicket}) => {
+const Ticket = ({ ticket, selectedTicket, setSelectedTicket }) => {
     // console.log(ticket)
     const { title, status, description, id, priority, customer, created_date } = ticket;
     const [isSelected, setIsSelected] = useState(false)
 
-   
+
 
     const handleSelected = (TicketsData) => {
         setIsSelected(true)
         toast('Ticket added successfully')
-setSelectedTicket([...selectedTicket, TicketsData])
+        setSelectedTicket([...selectedTicket, TicketsData])
     }
     return (
         <>
-        <div onClick={() => {handleSelected(ticket)}} className='max-w-[1200px] mx-auto border-2 mb-1 border-gray-200 rounded-xl p-4'>
-            <div className='flex justify-between'>
-                <h2 className='font-bold'>{title}</h2>
-                <button className='btn btn-soft btn-accent rounded-2xl'>{status}</button>
-            </div>
-            <p className='text-gray-400'>{description}</p>
-            <div className="last-line flex justify-between">
-                <div className='flex gap-3'>
-                    <span>{id}</span>
-                    <span>{priority}</span>
+            <div onClick={() => { handleSelected(ticket) }} className='max-w-[1200px] mx-auto border-2 mb-1 border-gray-200 rounded-xl p-2 md:p-4'>
+                <div className='md:flex justify-between'>
+                    <h2 className='font-bold'>{title}</h2>
+                    <button className={`btn rounded-2xl
+                    ${status === "Open"
+                        ?"btn-accent"
+                        :"btn-warning"
+                    }
+                    `}>{status}</button>
                 </div>
-                <div className='flex gap-3'>
-                    <span className='text-gray-500 font-semibold'>{customer}</span>
-                    <span className='flex gap3 items-center'>
-                        <span className='mr-1'><img src={dateImg} alt="" /></span>{created_date}<span></span>
-                    </span>
+                <p className='text-gray-400'>{description}</p>
+                <div className="last-line  md:flex justify-between">
+                    <div className='flex gap-3'>
+                        <span>{id}</span>
+                        <span className={`font-semibold ${
+    priority === "HIGH PRIORITY"
+      ? "text-red-500"
+      : priority === "MEDIUM PRIORITY"
+      ? "text-yellow-500"
+      : "text-green-500"
+  }`}>{priority}</span>
+                    </div>
+                    <div className='flex gap-3'>
+                        <span className='text-gray-500 font-semibold'>{customer}</span>
+                        <span className='flex gap3 items-center'>
+                            <span className='mr-1'><img src={dateImg} alt="" /></span>{created_date}<span></span>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-        {/* <TaskCompleted handleDelete={handleDelete}></TaskCompleted> */}
-       
+            {/* <TaskCompleted handleDelete={handleDelete}></TaskCompleted> */}
+
         </>
-        
+
     );
 };
 
